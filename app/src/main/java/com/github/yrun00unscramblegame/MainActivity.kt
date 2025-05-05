a3f132843c9c4753c4d466184c9c36f3b7c44bb4
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2025-05-05 08:46:09 +0400

    Feature/ug 04 main activity (#5)
    * Main Activity
    
    * Main Activity

--- app/src/main/java/com/github/yrun00unscramblegame/MainActivity.kt
+++ app/src/main/java/com/github/yrun00unscramblegame/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModel
+import com.github.yrun00unscramblegame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = MainViewModel()
+
+        binding.inputEditText.addTextChangedListener {
+            val uiState: MainUiState = viewModel.handleUserInput(text = it.toString())
+            uiState.update(binding = binding)
+        }
+
+        binding.checkButton.setOnClickListener {
+            val uiState: MainUiState = ViewModel.check
+            uiState.update(binding = binding)
+        }
+
+        binding.nextButton.setOnClickListener {
+            val uiState: MainUiState = ViewModel.next
+            uiState.update(binding = binding)
+        }
+
+        binding.skipButton.setOnClickListener {
+            val uiState: MainUiState = ViewModel.skip
+            uiState.update(binding = binding)
         }
     }
 }
